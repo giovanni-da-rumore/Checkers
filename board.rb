0340be7302c988d6fd48a15cff05415addd5098d
jeremy <jsl2184@gmail.com>
2015-04-02 12:09:55 -0400

    Make basic possible moves method for Piece class

--- board.rb
+++ board.rb
@@ -1,4 +1,13 @@
+#encoding: UTF-8
+
+require_relative 'piece'
+require 'colorize'
+
+
+
 class Board
+
+  attr_accessor :grid
 
 
   def initialize
@@ -11,8 +20,55 @@ class Board
   end
 
 
+  def [](pos)
+    x, y = pos[0], pos[1]
+    self.grid[x][y]
+  end
+
+
+  def []=(pos, piece)
+    x, y = pos[0], pos[1]
+    self.grid[x][y] = piece
+  end
+
+
+  def empty_space?(pos)
+    self[pos].nil?
+  end
+
+
+  def perform_slide(start_pos, end_pos)
+    piece = board[start_pos]
+    raise "Start Position empty" if piece.nil?
+
+    piece.moves
+  end
+
+
+    # raise "Invalid Move!"  unless
+    #   board[start_pos].moves.include?(end_pos)
+    #
+
+
+
 
 end
+
+
+
+board = Board.new
+piece = Piece.new([0,0], :black, board)
+piece2 = Piece.new([4,4], :red, board)
+
+puts piece.symbol
+p piece.possible_moves
+puts piece2.symbol
+p piece2.possible_moves
+
+
+
+
+
 
 
 
--- piece.rb
+++ piece.rb
@@ -3,27 +3,95 @@
 
 class Piece
 
-  attr_accessor :color, :king
+  MOVES = {
+    red:    [[-1, 1], [-1, -1]],
+    black:  [[1, 1], [-1, -1]],
+    king:   [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+  }
 
-  def initialize(color)
+  ATTACK_MOVES = {
+    red: [[-2, 2], [-2, -2]],
+    black: [[2, 2], [-2, -2]],
+    king: [[2, 2], [2, -2], [-2, 2], [-2, -2]]
+  }
+
+  attr_accessor :color, :king, :pos, :board, :moves
+
+  def initialize(pos, color, board)
+
+    @pos = pos
     @color = color
+    @board = board
     @king = false
+    @moves = []
   end
 
   def symbol
-    @color == :red ? '紅' : '黑'
+    return color == :red ? '紅' : '黑' unless king?
+    color == :red ? '朱' : '烏'
   end
 
   def king?
     king
   end
 
+  def crown
+    @king = true
+  end
+
+
+  def possible_moves
+    self.moves = normal_moves
+    moves.concat(attack_moves)
+  end
+
+  def normal_moves
+    self.moves = []
+    move_dirs.each_with_index do |(dx, dy), idx|
+      new_space = pos[0] + dx, pos[1] + dy
+      if in_board?(new_space) && board.empty_space?(new_space)
+        moves << new_space
+      end
+    end
+    moves
+  end
+
+  def attack_moves
+    attack_moves = []
+    attack_move_dirs.each_with_index do |(dx, dy), idx|
+      new_space = pos[0] + dx, pos[1] + dy
+      taken = move_dirs[idx]
+      if in_board?(new_space) && board.empty_space?(new_space)
+        next if board[taken].nil?
+          moves << new_space
+      end
+    end
+    attack_moves
+
+  end
+
+
+
+
+  def perform_jump
+  end
+
+
+
+  def move_dirs
+    return MOVES[color] unless king?
+    MOVES[king]
+  end
+
+  def attack_move_dirs
+    return ATTACK_MOVES[color] unless king?
+    ATTACK_MOVES[king]
+  end
+
+  def in_board?(pos)
+    pos[0].between?(0,9) && pos[1].between?(0,9)
+  end
+
+
+
 end
-
-
-
-piece = Piece.new(:black)
-piece2 = Piece.new(:red)
-
-puts piece.symbol
-puts piece2.symbol
